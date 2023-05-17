226d17d66da3a39c2292d3ae40582119a7c6d51b
Klaimand22 <clement.michellod@gmail.com>
2023-05-17 14:59:22 +0200

    emprunt

--- html/emprunter.php
+++ html/emprunter.php
@@ -28,59 +28,61 @@
     <p>Description : <?php echo $row['description'] ?></p>
     <p>Référence : <?php echo $row['reference'] ?></p>
     <p>Disponible : <?php echo $row['disponible'] ?></p>
+    <p>Image : <img src="<?php echo $row['image'] ?>" alt="image du boitier" width="200px"></p>
 
 
    <!-- selectionner un client  -->
 
 
     <form action="emprunter.php" method="POST">
-        <label for="client">Client</label>
-        <select name="client" id="client">
-            <?php
-            include_once "connexion.php";
-            $sql = "SELECT * FROM sae203_client";
-            $result = mysqli_query($CONNEXION, $sql);
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<option value='" . $row['id_client'] . "'>" . $row['nom'] . "</option>";
-            }
-            ?>
-        </select>
-        <br>
-        <label for="date_debut">Date de début</label>
-        <input type="date" name="date_debut" id="date_debut" required>
-        <br>
-        <label for="date_fin">Date de fin</label>
-        <input type="date" name="date_fin" id="date_fin" required>
-        <br>
-        <input type="hidden" name="id" value="<?php echo $id ?>">
-        <input type="hidden" name="categorie" value="<?php echo $nom_categorie ?>">
-        <button type="submit" name="submit">Emprunter</button>
-    </form>
-
-    <?php
-    if (isset($_POST['submit'])) {
-        $id_client = $_POST['client'];
-        $date_debut = $_POST['date_debut'];
-        $date_fin = $_POST['date_fin'];
-        $id_boitier = $_POST['id'];
-        $categorie = $_POST['categorie'];
-        $sql = "INSERT INTO sae203_emprunt (date_debut, date_fin, sae203_client_id_client, sae203_boitier_id_boitier) VALUES ('$date_debut', '$date_fin', '$id_client', '$id_boitier')";
+    <form action="emprunter.php" method="POST">
+    <label for="client">Client</label>
+    <select name="client" id="client">
+        <?php
+        include_once "connexion.php";
+        $sql = "SELECT * FROM sae203_client";
         $result = mysqli_query($CONNEXION, $sql);
-        if ($result) {
-            $sql_update = "UPDATE sae203_boitier SET disponible = 0 WHERE id_boitier = $id_boitier";
-            $result_update = mysqli_query($CONNEXION, $sql_update);
-            if ($result_update) {
-                echo "<h1>Emprunt effectué</h1>";
-                header("Location: $nom_categorie.php");
-            }
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<option value='" . $row['id_client'] . "'>" . $row['nom'] . "</option>";
+        }
+        ?>
+    </select>
+    <br>
+    <label for="date_debut">Date de début</label>
+    <input type="date" name="date_debut" id="date_debut" required>
+    <br>
+    <label for="date_fin">Date de fin</label>
+    <input type="date" name="date_fin" id="date_fin" required>
+    <br>
+    <input type="hidden" name="id" value="<?php echo $id ?>">
+    <input type="hidden" name="categorie" value="<?php echo $nom_categorie ?>">
+    <button type="submit" name="submit">Emprunter</button>
+</form>
+
+<?php
+require_once('connexion.php');
+if (isset($_POST['submit'])) {
+    $id_client = $_POST['client'];
+    $id_boitier = $_POST['id'];
+    $nom_categorie = $_POST['categorie'];
+    $date_debut = $_POST['date_debut'];
+    $date_fin = $_POST['date_fin'];
+    $sql = "INSERT INTO sae203_emprunt (id_emprunt, sae203_categorie_id_categorie, sae203_client_id_client, date_debut, date_fin) VALUES (NULL, $id_boitier, $id_client, '$date_debut', '$date_fin')";
+    $result = mysqli_query($CONNEXION, $sql);
+    if ($result) {
+        $sql_update = "UPDATE sae203_$nom_categorie SET disponible = 0 WHERE id_$nom_categorie = $id_boitier";
+        $result_update = mysqli_query($CONNEXION, $sql_update);
+        if ($result_update) {
+            echo "Le boitier a bien été emprunté";
+        } else {
+            echo "Erreur lors de l'emprunt";
         }
     }
-    ?>
+}
 
 
+?>
 
-
-    <!-- bouton retour -->
     <a href="<?php echo $nom_categorie ?>.php">Retour</a>
 
 
